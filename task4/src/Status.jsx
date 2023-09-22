95f0e8c8fb839ab06a23a8703796a0b42ff55b54
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-09-22 14:17:14 +0300

    online or offline

--- task4/src/Status.jsx
+++ task4/src/Status.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Online from './Online';
+import Offline from './Offline';
 
 class Status extends React.Component {
   render() {
-    const { isOffline } = this.props;
-    if (isOffline === false) {
-      return <Offline />;
-    } else {
+    const { isOnline } = this.props;
+    if (isOnline) {
       return <Online />;
+    } else {
+      return <Offline />;
     }
   }
 }
